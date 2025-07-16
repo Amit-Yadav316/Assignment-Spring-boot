43699b711eed3b5ae2af2363fc375a7863d08a55
Amit-Yadav316 <yadavineet9198@gmail.com>
2025-07-16 14:57:11 +0530

    add TestElasticsearchContainer class for Elasticsearch integration testing

---
+++ src/test/java/com/example/course_search/TestElasticsearchContainer.java
@@ -0,0 +1,18 @@
+package com.example.course_search;
+
+import org.testcontainers.elasticsearch.ElasticsearchContainer;
+import org.testcontainers.utility.DockerImageName;
+
+public class TestElasticsearchContainer extends ElasticsearchContainer {
+    private static final DockerImageName IMAGE = DockerImageName
+            .parse("elasticsearch:8.13.4")
+            .asCompatibleSubstituteFor("docker.elastic.co/elasticsearch/elasticsearch");
+
+    public TestElasticsearchContainer() {
+        super(IMAGE);
+        addExposedPort(9200);
+        addEnv("discovery.type", "single-node");
+        addEnv("xpack.security.enabled", "false");
+        addEnv("cluster.name", "elasticsearch");
+    }
+}
